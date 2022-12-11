5567f373b3135c0a3f731ba1a9dbd64bf742f276
Alicia Taing <taingalicia@gmail.com>
2022-12-11 14:25:16 -0800

    Modeled Sundial and it's scenenode hierarchy

---
+++ Assets/Scripts/Model/Scr_Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Controller : MonoBehaviour
+{
+    public SceneNode RootNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Assert(RootNode != null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Matrix4x4 m = Matrix4x4.identity;
+        RootNode.CompositeXform(ref m);
+    }
+
+    public SceneNode GetRootNode() { return RootNode; }
+    
+}
